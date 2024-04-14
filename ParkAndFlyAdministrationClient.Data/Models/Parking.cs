03765c4a7b7f24e2c3cb383cebddc0b0bfef8f8c
padjal <padjal136@gmail.com>
2024-04-14 17:26:45 +0300

    Add parking service

---
+++ ParkAndFlyAdministrationClient.Data/Models/Parking.cs
@@ -0,0 +1,15 @@
+﻿namespace ParkAndFlyAdministrationClient.Data.Models
+{
+    internal class Parking
+    {
+        public int Id { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string Phone { get; set; }
+        public int MaxCars { get; set; }
+    }
+}
---
+++ ParkAndFlyAdministrationClient.Data/Services/ParkingService.cs
@@ -0,0 +1,41 @@
+﻿using ParkAndFlyAdministrationClient.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkAndFlyAdministrationClient.Data.Services
+{
+    internal class ParkingService : IParkingService
+    {
+        private readonly HttpClient _httpClient;
+        public ParkingService(HttpClient httpClient) {
+            _httpClient = httpClient;
+        }
+
+        public async Task<bool> AddParkingAsync(Parking parking)
+        {
+            //var response = await _httpClient.GetAsync("api/v1/parking");
+
+            await Task.Delay(300);
+
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Parking>> GetParkingsAsync()
+        {
+            await Task.Delay(300);
+
+            return new List<Parking>() { new Parking() {Name = "New parking 1" }, new Parking() { Name = "New parking 2" } };
+        }
+    }
+
+    internal interface IParkingService
+    {
+        Task<IEnumerable<Parking>> GetParkingsAsync();
+
+        Task<bool> AddParkingAsync(Parking parking);
+    }
+}
